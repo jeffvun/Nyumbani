8ee3cfc74abd2795d11efcc003dfcc567b20cd14
jeffvun <64653401+jeffvun@users.noreply.github.com>
2021-11-02 09:21:27 +0300

    Added Listings Module

--- app/Models/PropertyListingModel.php
+++ app/Models/PropertyListingModel.php
@@ -13,11 +13,12 @@ class PropertyListingModel extends Model{
 
     public function getPropertyListing($id){
         //Initializing query builder
-        $builder = $this->db->table('tbl_listing');
-        $builder->join("tbl_users", "tbl_listings.tenantID = tbl_users.userID",  "left");
+        $builder = $this->db->table('tbl_listings');
+        $builder->join("tbl_property", "tbl_listings.propertyID = tbl_property.propertyID",  "left");
+        $builder->join("tbl_users", "tbl_property.tenantID = tbl_users.userID",  "left");
         $builder->join("tbl_propertydetails", "tbl_property.propertyID = tbl_propertydetails.propertyID",  "left");
         $builder->join("tbl_propertymedia", "tbl_propertydetails.propertyID = tbl_propertymedia.propertyID",  "left");
-        $builder->where("ownerID", $id);
+        $builder->where("tbl_property.ownerID", $id);
         $query=$builder->get();
 
         return $query->getResult();
