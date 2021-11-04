8b3ce2354ebd52c22baa60f8d599c1f0e2e18fac
jeffvun <64653401+jeffvun@users.noreply.github.com>
2021-11-04 18:02:08 +0300

    Update on Mergin Conflicts on Listing Module

--- app/Controllers/Applications.php
+++ app/Controllers/Applications.php
@@ -11,14 +11,15 @@ class Applications extends BaseController{
         $application = new ApplicationsModel($db); 
         $results = $application->getApplications(1);
 
-        // return json_encode($results);
+        return json_encode($results);
 
         #For Testing Purporse
-        echo "<pre>";
-        print_r($results);
-        echo "</pre>"; 
-        $data = ['ViewApplications'=>json_encode($results)];
+        // echo "<pre>";
+        // print_r($results);
+        // echo "</pre>"; 
+        
+        // $data = ['ViewApplications'=>json_encode($results)];
 
-        return view('ViewApplications', $data);
+        // return view('ViewApplications', $data);
     }
 }
--- app/Controllers/Requests.php
+++ app/Controllers/Requests.php
@@ -6,17 +6,19 @@ use App\Models\RequestsModel;
 class Requests extends BaseController{
 
     public function index(){
+        $db = db_connect();
         $request = new RequestsModel(); 
         $results = $request->getRequests(1); 
 
-        // return json_encode($results);
+        return json_encode($results);
         
         #For Testing Purporse
-        echo "<pre>";
-        print_r($results);
-        echo "</pre>"; 
+        // echo "<pre>";
+        // print_r($results);
+        // echo "</pre>"; 
 
-        $data = ['ViewRequests'=>json_encode($results)];
-        return view('ViewRequests', $data);
+        // $data = ['ViewRequests'=>json_encode($results)];
+
+        // return view('ViewRequests', $data);
     }
 }
